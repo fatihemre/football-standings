f6e64226c78b9b716d3d5931725fdf393fbf1129
Fatih Emre Öner <emre@vision-org.com>
2022-08-21 17:32:24 +0300

    Anasayfada aktif haftanın görünmesi sağlandı.

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace Standings\Controller;
 
+use Standings\Model\ActiveWeek;
+use Standings\Model\ActiveWeekEntity;
 use Standings\Model\FixtureView;
 
 class HomeController extends Controller
@@ -9,7 +11,8 @@ class HomeController extends Controller
     public function index()
     {
         $fixture = new FixtureView();
-        $matches = $fixture->getMatches(1, 1);
+
+        $matches = $fixture->getMatches(1, (new ActiveWeek)->get()->active_week);
 
         return $this->view('index', ['matches' => $matches]);
     }
