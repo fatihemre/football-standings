665e7ca8decaeee70c2c8b58b42438de5adb7afc
Fatih Emre <emre@vision-org.com>
2022-08-28 23:31:24 +0300

    exists method removed, getUser method added to User model. Password checker moved to AuthController

--- app/Model/User.php
+++ app/Model/User.php
@@ -9,15 +9,12 @@ class User
 {
     use Database;
 
-    public function exists($email, $password)
+    public function getUser($email, $password)
     {
         $sth = $this->connection->prepare("SELECT * FROM users WHERE email=:email");
         $sth->execute(['email'=>$email]);
         $sth->setFetchMode(\PDO::FETCH_CLASS, UserEntity::class);
-        $user = $sth->fetch();
-
-        return $user ? (password()->verify($user->password, $password) ? $user : false) : false;
-
+        return $sth->fetch();
     }
 
 }
