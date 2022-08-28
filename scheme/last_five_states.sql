239bdd0fccb8a6923965798155971cf0d258b4c4
Fatih Emre <emre@vision-org.com>
2022-08-28 22:36:47 +0300

    All views, tables and insert queries updated.

--- scheme/last_five_states.sql
+++ scheme/last_five_states.sql
@@ -1,31 +1,22 @@
--- -------------------------------------------------------------
--- TablePlus 4.8.3(439)
---
--- https://tableplus.com/
---
--- Database: football
--- Generation Time: 2022-08-21 18:46:10.0710
--- -------------------------------------------------------------
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `last_five_states` AS select `fixture_view`.`id` AS `id`,`fixture_view`.`home_team_id` AS `home_team_id`,`fixture_view`.`away_team_id` AS `away_team_id`,`fixture_view`.`home_team_score` AS `home_team_score`,`fixture_view`.`away_team_score` AS `away_team_score`,(case when (`fixture_view`.`home_team_score` > `fixture_view`.`away_team_score`) then 'home_win' when (`fixture_view`.`away_team_score` > `fixture_view`.`home_team_score`) then 'away_win' else 'draw' end) AS `state` from `fixture_view` where (`fixture_view`.`match_day` < now()) order by `fixture_view`.`match_day` desc;
-
-
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+CREATE VIEW `last_five_states` AS
+SELECT
+    `fixture_view`.`id` AS `id`,
+    `fixture_view`.`home_team_id` AS `home_team_id`,
+    `fixture_view`.`away_team_id` AS `away_team_id`,
+    `fixture_view`.`home_team_score` AS `home_team_score`,
+    `fixture_view`.`away_team_score` AS `away_team_score`,
+    (
+        CASE
+            WHEN (`fixture_view`.`home_team_score` > `fixture_view`.`away_team_score`)
+                THEN 'home_win'
+            WHEN (`fixture_view`.`away_team_score` > `fixture_view`.`home_team_score`)
+                THEN 'away_win'
+            ELSE 'draw'
+        END
+    ) AS `state`
+FROM
+    `fixture_view`
+WHERE
+    `fixture_view`.`match_day` < NOW()
+ORDER BY
+    `fixture_view`.`match_day` DESC;
